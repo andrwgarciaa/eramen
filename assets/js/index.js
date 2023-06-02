ed946345b7796077f417516c14321be37aa63b01
andrwgarciaa <andrwgarciaa@gmail.com>
2023-06-02 23:35:23 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,127 @@
+const play = document.querySelector('#play-button');
+const pause = document.querySelector('#pause-button');
+const restart = document.querySelector('#restart-button');
+const progress = document.querySelector('.progress-fill');
+const breakpoint = document.querySelectorAll('.progress-breakpoint');
+const news = document.querySelectorAll('.news');
+let progressWidth = 0;
+let interval;
+
+const toggleHidden = (active, hidden) => {
+	active.classList.add('active');
+	active.classList.remove('hidden');
+	hidden.classList.add('hidden');
+	hidden.classList.remove('active');
+};
+
+const navigateProgress = (active, hidden) => {
+	active.classList.add('active');
+	active.classList.remove('hidden');
+	hidden.map((i) => {
+		i.classList.add('hidden');
+		i.classList.remove('active');
+	});
+};
+
+// progress bar
+const playProgressBar = () => {
+	checkProgress();
+	if (progressWidth === 100) {
+		progressWidth = 0;
+		progress.style.transition = 'none';
+		progress.style.width = progressWidth + '%';
+		checkProgress();
+		toggleHidden(pause, play);
+	}
+	clearInterval(interval);
+	toggleHidden(pause, play);
+	const frame = () => {
+		if (progressWidth >= 100) {
+			progressWidth = 100;
+			toggleHidden(play, pause);
+			clearInterval(interval);
+		} else {
+			progressWidth += 5;
+			progress.style.width = progressWidth + '%';
+			checkProgress();
+		}
+	};
+	interval = setInterval(frame, 1000);
+};
+
+const pauseProgressBar = () => {
+	clearInterval(interval);
+};
+
+const restartProgressBar = () => {
+	for (let i = 0; i < news.length; i++) {
+		if (i === 0) {
+			news[i].classList.add('active');
+			news[i].classList.remove('hidden');
+		} else {
+			news[i].classList.add('hidden');
+			news[i].classList.remove('active');
+		}
+	}
+	progressWidth = 0;
+	progress.style.transition = 'none';
+	progress.style.width = progressWidth + '%';
+	clearInterval(interval);
+	playProgressBar();
+};
+
+const skipProgressTo = (i, position) => {
+	clearInterval(interval);
+	navigateProgress(
+		news[i],
+		Array.from(news).filter((i) => {
+			return i.classList.contains('active');
+		})
+	);
+	console.log(i, position);
+	progressWidth = position;
+	progress.style.width = progressWidth + '%';
+	checkProgress();
+	playProgressBar();
+};
+
+for (let i = 0; i < breakpoint.length; i++) {
+	breakpoint[i].addEventListener('click', () => {
+		let position;
+		if (i === 0) position = 5;
+		else position = i * 25;
+		skipProgressTo(i + 1, position);
+	});
+}
+
+const checkProgress = () => {
+	if (progressWidth === 0) {
+		toggleHidden(news[0], news[5]);
+	} else if (progressWidth === 5) {
+		toggleHidden(news[1], news[0]);
+	} else if (progressWidth === 30) {
+		toggleHidden(news[2], news[1]);
+	} else if (progressWidth === 55) {
+		toggleHidden(news[3], news[2]);
+	} else if (progressWidth === 80) {
+		toggleHidden(news[4], news[3]);
+	} else if (progressWidth === 100) {
+		toggleHidden(news[5], news[4]);
+	}
+};
+
+// progress bar buttons
+play.addEventListener('click', () => {
+	playProgressBar();
+});
+
+pause.addEventListener('click', () => {
+	pauseProgressBar();
+	toggleHidden(play, pause);
+});
+
+restart.addEventListener('click', restartProgressBar);
+
+document.addEventListener('load', playProgressBar());
+
+// check progress
