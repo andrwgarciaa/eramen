3824494157fd4d7df22f90cbb046aad9aab0d69d
andrwgarciaa <andrwgarciaa@gmail.com>
2023-06-09 10:22:14 +0700

    updates

--- assets/js/promotions.js
+++ assets/js/promotions.js
@@ -3,4 +3,5 @@ const adsContainer = document.querySelector('.ads-container');
 adsContainer.addEventListener('wheel', (e) => {
 	e.preventDefault();
 	adsContainer.scrollLeft += e.deltaY / 2;
+	adsContainer.scrollLeft += e.deltaX / 2;
 });
